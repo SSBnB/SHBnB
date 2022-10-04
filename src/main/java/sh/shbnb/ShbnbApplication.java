e33319d9cd94c70c4477bca3e0700738d90e2287
xxeol2 <jhj01177@naver.com>
2022-10-04 15:15:27 +0900

    [INIT] init project

---
+++ src/main/java/sh/shbnb/ShbnbApplication.java
@@ -0,0 +1,13 @@
+package sh.shbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShbnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sh/shbnb/ShbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.shbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
